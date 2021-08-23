1568628e1413867bb8333a96ad0c0f83aadaa014
Spencer <sburke17@emich.edu>
2021-08-23 18:14:09 -0400

    Added bones to schedule page

--- schedule.php
+++ schedule.php
@@ -5,7 +5,23 @@
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    <h1>Schedules</h1>
-    
+    <div class="p-lg-5 p-2">
+        <div class="row text-center">
+            <div class="col-lg-3 col-1"></div>
+            <div class="col-lg-6 col-10">
+                <h1>Schedules</h1>
+                <h4><a href="schedule.php">Home -></a></h4>
+            </div>
+        </div>
+        <div class="row largeText text-center">
+            <div class="col-1"></div>
+            <div class="col-lg-4 col-10">
+
+                
+
+            </div>
+            
+        </div>
+    </div>
 </body>
 </html>
